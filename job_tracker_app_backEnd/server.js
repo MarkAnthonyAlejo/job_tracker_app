08ca38f853026234a095bfaf494089416b81a786
Mark Anthony Alejo <mark.anthony552@yahoo.com>
2025-06-16 10:43:28 -0700

    Updated server.js

--- job_tracker_app_backEnd/server.js
+++ job_tracker_app_backEnd/server.js
@@ -17,24 +17,26 @@ app.get("/", (req, res) => {
 //  "http://localhost:5173",
 //       "https://job-tracker-app-backend.onrender.com"
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://weather-app-weld-two-37.vercel.app/",
-  "https://job-tracker-app-seven.vercel.app/",
-];
+// const allowedOrigins = [
+//   "http://localhost:3000",
+//   "https://weather-app-weld-two-37.vercel.app/",
+//   "https://job-tracker-app-seven.vercel.app/",
+// ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
+
+app.use(cors());
 
 //Routes
 app.use(express.json());
